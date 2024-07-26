761840e22d2ed1b60596cfdd3b8dcc8cbceb275f
gaoguoxing <nanyiniu76@gmail.com>
2024-07-26 14:35:48 +0800

    add db and table serialize

--- src/db.rs
+++ src/db.rs
@@ -1,12 +1,13 @@
-use anyhow::Result;
-use std::path::PathBuf;
+use anyhow::{anyhow, Context, Result};
+use serde::{Deserialize, Serialize};
+use std::{fmt::format, fs, path::PathBuf};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct DataBase {
     pub host: String,
-    pub path: PathBuf,
-    pub table: Vec<Table>,
+    pub tables: Vec<Table>,
 }
-
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Table {
     pub name: String,
     pub path: String,
@@ -15,18 +16,70 @@ pub struct Table {
 }
 
 impl DataBase {
-    pub fn get_connect() -> Result<()> {
+    pub fn get_connect(self) -> Result<()> {
         // 打开文件目录
+        let connect_dir = "../";
+        let pb = PathBuf::from(connect_dir);
+        pb.join(self.host).with_extension("txt");
         // 根据Host创建的文件内容
+        match std::fs::read(pb) {
+            Ok(bytes) => {
+                todo!()
+            }
+            Err(e) => return Err(anyhow!("dbError:{}", e)),
+        }
         // read 出来
         Ok(())
     }
 
-    pub fn create_db() -> Result<()> {
-        todo!()
+    pub fn create_db(host: String, tables: Vec<Table>) -> Result<DataBase> {
+        let db: DataBase = DataBase { host, tables };
+        Ok(db)
     }
 
     pub fn update_db() -> Result<()> {
         todo!()
+    }
+
+    pub fn serialize(dbs: &Vec<DataBase>) -> Result<Vec<u8>> {
+        (|| -> bincode::Result<_> {
+            let magic_number = "JPS";
+            let magic_number_size = bincode::serialized_size(magic_number)?;
+            let content_size = bincode::serialized_size(&dbs)?;
+            let buffer_size = magic_number_size + content_size;
+            let mut buffer = Vec::with_capacity(buffer_size as usize);
+            // 这里的version
+            bincode::serialize_into(&mut buffer, magic_number)?;
+            bincode::serialize_into(&mut buffer, dbs)?;
+            Ok(buffer)
+        })()
+        .context("error")
+    }
+
+    pub fn deserialize(buffer: &Vec<u8>) -> Result<Vec<DataBase>> {
+        let magic_number = "JPS";
+        let magic_number_size = bincode::serialized_size(magic_number)?;
+        if buffer.len() < magic_number_size as usize {
+            return Err(anyhow!("error"));
+        }
+        let (magic_number, content) = buffer.split_at(magic_number_size as usize);
+        // 分别反序列化
+        let db: Vec<DataBase> = match bincode::deserialize(&magic_number).unwrap() {
+            "JPS" => bincode::deserialize(&content)?,
+            _ => return Err(anyhow!("error version code")),
+        };
+        Ok(db)
+    }
+}
+
+impl Table {
+    pub fn new(name: String, path: String, rank: f64, last_accessed: u64) -> Result<Table> {
+        let t = Table {
+            name,
+            path,
+            rank,
+            last_accessed,
+        };
+        Ok(t)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,32 @@ pub fn main() -> ExitCode {
         Err(e) => ExitCode::FAILURE,
     }
 }
+#[cfg(test)]
+pub mod test {
+    use serde::Serialize;
+
+    use crate::db::{DataBase, Table};
+
+    #[test]
+    pub fn test_bincode() {
+        // The object that we will serialize.
+        let target: Option<String> = Some("hello world".to_string());
+
+        let encoded: Vec<u8> = bincode::serialize(&target).unwrap();
+        println!("{:?}", encoded);
+        let decoded: Option<String> = bincode::deserialize(&encoded[..]).unwrap();
+        println!("{:?}", decoded);
+        assert_eq!(target, decoded);
+    }
+    #[test]
+    pub fn db_serialize() {
+        let t = Table::new(String::from("t1"), String::from("path"), 1.0, 2).unwrap();
+        let tables = vec![t];
+        let db: DataBase = DataBase::create_db(String::from("ver"), tables).unwrap();
+        let dbs = vec![db];
+        let encoded = DataBase::serialize(&dbs).unwrap();
+        println!("{:?}", encoded);
+        let decoded = DataBase::deserialize(&encoded).unwrap();
+        println!("{:?}", decoded);
+    }
+}
